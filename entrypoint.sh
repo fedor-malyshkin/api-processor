95fa281bcc71699f3fe4408daf063b1dff9d3195
fedor.malyshkin <fedor.malyshkin@gmail.com>
2022-03-26 02:16:53 +0400

    e

--- entrypoint.sh
+++ entrypoint.sh
@@ -100,6 +100,7 @@ if [ "$branch" = "master" -o "$branch" = "main"  ] ; then
   git fetch origin develop:refs/remotes/origin/develop  && \
   git checkout  develop && \
   echo $version > $GITHUB_WORKSPACE/VERSION && \
+  git commit -a -m "[API Processor] New API for version $version" && \
   git push --tags --repo="${remote_repo}" origin HEAD
 fi
 
