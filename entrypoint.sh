a9784a409cbc5269ae0c91d031a91fcc93398def
Fedor Malyshkin <475889+fedor-malyshkin@users.noreply.github.com>
2022-03-27 19:02:15 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,29 +22,20 @@ PROTO_SPEC_FILE=api.proto
 ANNOTATION_PROTO_DIR=$PROCESSOR_DIR/protos
 OPEN_API_OUTPUT_DIR=$GITHUB_WORKSPACE/open-api
 OPEN_API_SPEC_FILE=$OPEN_API_OUTPUT_DIR/openapi.yaml  
-GO_OUTPUT_DIR=$GITHUB_WORKSPACE/go
-GO_GRPC_OUTPUT_DIR=$GITHUB_WORKSPACE/go-rpc
+GO_OUTPUT_DIR=$GITHUB_WORKSPACE/go/api
+GO_GRPC_OUTPUT_DIR=$GITHUB_WORKSPACE/go-rpc/api
 JS_OUTPUT_DIR=$GITHUB_WORKSPACE/js-api
 
 rm -r -d -f $OPEN_API_OUTPUT_DIR $GO_OUTPUT_DIR $GO_GRPC_OUTPUT_DIR $JS_OUTPUT_DIR
 mkdir -p $OPEN_API_OUTPUT_DIR $GO_OUTPUT_DIR $GO_GRPC_OUTPUT_DIR $JS_OUTPUT_DIR
 
 root_dir=$GITHUB_WORKSPACE
-# Open API
-cd $root_dir
-if protoc $PROTO_SPEC_FILE -I$PROTO_SPEC_DIR -I$ANNOTATION_PROTO_DIR  --openapi_out=$OPEN_API_OUTPUT_DIR; then
-  # do nothing
-  cd $root_dir
-else
-     exit $?
-fi
-
 
 # Go
 cd $root_dir
 if protoc $PROTO_SPEC_FILE -I$PROTO_SPEC_DIR -I$ANNOTATION_PROTO_DIR --go_out=$GO_OUTPUT_DIR  --go_opt=paths=source_relative; then
   cd $GO_OUTPUT_DIR
-  go mod init "github.com/"$GITHUB_REPOSITORY"/go"
+  go mod init "github.com/"$GITHUB_REPOSITORY"/go/api"
   go mod tidy
 else
      exit $?
@@ -56,17 +47,28 @@ cd $root_dir
 if protoc  $PROTO_SPEC_FILE -I$PROTO_SPEC_DIR -I$ANNOTATION_PROTO_DIR --go_out=$GO_GRPC_OUTPUT_DIR --go_opt=paths=source_relative \
     --go-grpc_out=$GO_GRPC_OUTPUT_DIR --go-grpc_opt=paths=source_relative; then
   cd $GO_GRPC_OUTPUT_DIR
-  go mod init "github.com/"$GITHUB_REPOSITORY"/go-rpc"
+  go mod init "github.com/"$GITHUB_REPOSITORY"/go-rpc/api"
   go mod tidy
 else
      exit $?
 fi
 
+
+version=`cat $GITHUB_WORKSPACE/VERSION`
+version=`$PROCESSOR_DIR/semver.sh bump prerel beta. $version`
+
 # JS
 cd $root_dir
-if java -jar $PROCESSOR_DIR/openapi-generator-cli.jar generate -i $OPEN_API_SPEC_FILE -g typescript-axios -o $JS_OUTPUT_DIR; then
-  cd $JS_OUTPUT_DIR
-  # do smth with npm
+if npx protoc $PROTO_SPEC_FILE -I$PROTO_SPEC_DIR -I$ANNOTATION_PROTO_DIR --ts_out $JS_OUTPUT_DIR; then
+echo ' << EOF
+{
+  "name": "${GITHUB_REPOSITORY##*/}",
+  "version": "$version",
+  "description": "",
+  "keywords": [],
+  "author": "",
+  "license": "ISC"
+}'EOF > $JS_OUTPUT_DIR/package.json
 else
      exit $?
 fi
@@ -81,9 +83,11 @@ if [ "$branch" = "develop"  ] ; then
   echo $version > $GITHUB_WORKSPACE/VERSION
   git config user.email "api-processor@example.com"  && \
   git config user.name "API Processor"  && \
-  git add -A  && \
+  git add $GO_GRPC_OUTPUT_DIR'/**' && \
+  git add $GO_OUTPUT_DIR'/**' && \
   git commit -a -m "[API Processor] New API for version $version"  && \
-  git tag $version  && \
+  git tag "go/api/v$version"  && \
+  git tag "go-rpc/api/v$version"  && \
   git push --tags --repo="${remote_repo}" origin HEAD
 fi
 
@@ -93,10 +97,13 @@ if [ "$branch" = "master" -o "$branch" = "main"  ] ; then
   echo $version > $GITHUB_WORKSPACE/VERSION
   git config user.email "api-processor@example.com"  && \
   git config user.name "API Processor" && \
-  git add -A && \
+  git add $GO_GRPC_OUTPUT_DIR'/**' && \
+  git add $GO_OUTPUT_DIR'/**' && \
   git commit -a -m "[API Processor] New API for version $version" && \
-  git tag $version && \
+  git tag "go/api/v$version"  && \
+  git tag "go-rpc/api/v$version"  && \
   git push  --tags --repo="${remote_repo}" origin HEAD && \
+  git clean -d -f && \
   git fetch origin develop:refs/remotes/origin/develop  && \
   git checkout  develop && \
   echo $version > $GITHUB_WORKSPACE/VERSION && \
